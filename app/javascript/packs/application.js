4eed9dba4676afad0aa2fb34a48d9c55cb03fed7
Alexandre Trindade <alexandredct@gmail.com>
2021-04-26 16:18:28 -0300

    feat(views/bootstrap): Installing bootstrap

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -1,4 +1,4 @@
-// This file is automatically compiled by Webpack, along with any other files
+// This fbootstrapile is automatically compiled by Webpack, along with any other files
 // present in this directory. You're encouraged to place your actual application logic in
 // a relevant structure within app/javascript and only use these pack files to reference
 // that code so it'll be compiled.
@@ -7,6 +7,10 @@ import Rails from "@rails/ujs"
 import Turbolinks from "turbolinks"
 import * as ActiveStorage from "@rails/activestorage"
 import "channels"
+
+//= require jquery3
+//= require popper
+//= require bootstrap-sprockets
 
 Rails.start()
 Turbolinks.start()
