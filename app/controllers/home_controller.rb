56f00b5b0e183bbd71c66eabb443b537b4da78f8
Alexandre Trindade <alexandredct@gmail.com>
2021-04-27 11:00:55 -0300

    feat(lists/delete_all): Creating a button to delete all items

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -2,6 +2,11 @@ class HomeController < ApplicationController
 
   def trash_em
     @trash_em = List.where(:completed => true).destroy_all
-    redirect_to list_url, notice: "All marked items were successfully deleted."
+    redirect_to lists_url, notice: "All marked items were successfully deleted."
+  end
+
+  def trash_em_all
+    @trash_em_all = List.all.destroy_all
+    redirect_to lists_url, notice: "All items were successfully deleted."
   end
 end
